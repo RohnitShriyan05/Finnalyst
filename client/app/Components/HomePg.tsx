55f76bf3d94f93c1607c6374e2429a86f78eda2b
rvs <99482544+RohnitShriyan05@users.noreply.github.com>
2024-01-28 15:44:17 +0530

    added alpha vantage api call and chartjs

--- client/app/Components/HomePg.tsx
+++ client/app/Components/HomePg.tsx
@@ -1,14 +1,37 @@
+"use client"
 import { useEffect } from "react";
-import {axios} from axios;
-
+import Axios from "axios";
+import Chart from "react-google-charts";
+export const data = [
+    ["day", "a", "b", "c", "d"],
+    ["Mon", 1, 10, 30  , 45],
+    ["Tue", 31, 38, 55, 66],
+    ["Wed", 50, 55, 77, 80],
+    ["Thu", 50, 77, 66, 77],
+    ["Fri", 15, 66, 22, 68],
+  ];
+  
+  export const options = {
+    legend: "none",
+  };
+  
 export function HomePg() {
-    useEffect(()=>{
-        axios.get("https://api.upstox.com/v2/historical-candle/:instrument_key/:interval/:to_date/:from_date")
-    },[]);
+    useEffect(() => {
+        Axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=NVDA&interval=5min&apikey=7MD0IF5H889QIZ0C')
+            .then((res) => {
+                console.log(res.data);
+            })
+    }, []);
     return (
         <div className="fixed h-full w-full text-black text-3xl flex items-center justify-center">
             <div className="w-1/2 h-full bg-white">
-                
+                <Chart
+                    chartType="CandlestickChart"
+                    width="100%"
+                    height="400px"
+                    data={data}
+                    options={options}
+                />
             </div>
             <div className="w-1/2 h-full flex flex-col ">
                 <p>NEWS TODAY</p>
--- server/index.js
+++ server/index.js
@@ -12,7 +12,22 @@ app.get("/" , (req,res)=>{
     res.send("hello");
 })
 
+var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=NVDA&interval=5min&apikey=7MD0IF5H889QIZ0C';
 
+app.get({
+    url: url,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      // data is successfully parsed as a JSON object:
+      console.log(data);
+    }
+});
 app.listen(8000, ()=>{
     console.log("server running on: http://localhost:8000")
 })
